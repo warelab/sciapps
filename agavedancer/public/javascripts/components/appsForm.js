9515863eb18a2c5dae257d12b47c47b941381685
zhlu9890 <zhenyuan.lu@gmail.com>
2015-11-21 01:42:01 -0500

    appsParam is working

--- agavedancer/public/javascripts/components/appsForm.js
+++ agavedancer/public/javascripts/components/appsForm.js
@@ -2,8 +2,9 @@
 
 import React from 'react';
 import _ from 'lodash';
-import {Panel, Input} from 'react-bootstrap';
+import {Panel, Input, ButtonInput} from 'react-bootstrap';
 import AppsParam from './appsParam.js';
+
 
 function sortByOrder(unsorted) {
 	return _.sortBy(unsorted, function(item) {
@@ -27,13 +28,17 @@ var AppsForm=React.createClass({
 		var header="New job using application";
 		if (appDetail && undefined !== appDetail.name) {
 			header+=" " + appDetail.name + ":";
+			let sortedParams=sortByOrder(appDetail.parameters);
+			app_form=sortedParams.map(function(param) {
+				return(<AppsParam data={param} />);
+			});
 		}
-		var app_form=(
+		return (
 			<Panel header={header}>
 				{app_form}
+				<ButtonInput type="submit" value="Submit" />
 			</Panel>
 		);
-		return app_form;
 	}
 });
 
--- agavedancer/public/javascripts/components/appsParam.js
+++ agavedancer/public/javascripts/components/appsParam.js
@@ -11,20 +11,39 @@ const AgaveAppsParam=React.createClass({
 	handleChange: function(event) {
 		this.setState({value: event.target.value});
 	},
-	buildAgaveAppsParamProps: function(param) {
+	buildAgaveAppsSelectOption: function(option) {
+		var optionChild, optionProps;
+		if (_.isString(option)) {
+			optionProps={value: option};
+			optionChild=option;
+		} else {
+			option=_.flatten(_.pairs(option));
+			optionProps={value: option[0]};
+			optionChild=option[1];
+		}
+		return (
+			<option {...optionProps}>
+				{optionChild}
+			</option>
+		);
+	},
+	buildAgaveAppsParam: function(param) {
+		let markup, options, isSelect=false;
 		let props={
+			key: 'parameters.' + param.id,
 			value: this.state.value
 		};
 		if (! param.value.visible) {
 			props.type='hidden';
 		} else {
 			props=_.assign(props, {
+				onChange: this.handleChange,
 				label: param.details.label,
 				placeholder: param.value.default,
 				help: param.details.description
 			});
 			let addProps;
-			switch (param.type) {
+			switch (param.value.type) {
 				case 'bool':
 				case 'boolean':
 				case 'flag':
@@ -33,9 +52,14 @@ const AgaveAppsParam=React.createClass({
 						checked: this.state.value
 					};
 					break;
+				case 'enumeration':
+					addProps={
+						type: 'select'
+					};
+					isSelect=true;
+					break;
 				case 'number':
 				case 'string':
-				case 'enumeration':
 					addProps={
 						type: 'text'
 					};
@@ -43,13 +67,25 @@ const AgaveAppsParam=React.createClass({
 				default:
 			}
 			props=_.assign(props, addProps);
+			if (isSelect) {
+				options=param.value.enum_values.map(this.buildAgaveAppsSelectOption);
+				markup=(
+					<Input {...props}>
+						{options}
+					</Input>
+				);
+			} else {
+				markup=(<Input {...props} />);
+			}
 		}
+		return markup;
 	},
 	render: function() {
-		let props=this.buildAgaveAppsParamProps(this.props.data);
-		return (
-			<Input {...props} />
-		);
+		let markup=this.buildAgaveAppsParam(this.props.data);
+		return markup;
+		//return (
+		//	<Input {...props} />
+		//);
 	}
 });
 
