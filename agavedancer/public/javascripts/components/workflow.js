de9b981777c29439dc5fd4714639dd3c63632cf1
liyawang <liya.wang@brie.cshl.edu>
2016-03-31 19:32:14 -0400

    Further updated workflow page

--- agavedancer/public/javascripts/components/workflow.js
+++ agavedancer/public/javascripts/components/workflow.js
@@ -7,21 +7,24 @@ const Workflow=React.createClass({
 		return (
 			<div className="welcome">
                         <div className="section">
-			Automatic workflow chains individual apps together. The workflow constructed here not only reduces manual interactions with each individual app but also optimizes data transfer among intermediate steps. The inputs-outputs relationship among subsequent steps is built by running each app in the workflow once (see more details below).<br /><br />
-                        A workflow can be built by following steps:<br />
-                        1. Run each app used in the workflow at least once<br />
-                        2. Whenever possible, feed the outputs from the right History column as input for subsequent steps<br />
-                        3. Check whether outputs from each step are as expected; fix the error and repeat if not<br />
-                        4. Follow this link to build a workflow<br />
-                        <br />
+			Automatic workflow chains individual apps together. The workflow avoids manual submission of each individual job, reduces data transfer among intermediate steps, and further optimizes computation through advanced workflow engines. The inputs-outputs relationship among subsequent steps is automatically captured after running each app in the workflow once (by following step 2 below).<br /><br />
+                        The workflow is built by following steps:<br />
+			<ol className="list">
+                        <li>Run each app used in the workflow at least once</li>
+                        <li>Whenever possible, feed the outputs from the right History column as input for subsequent steps</li>
+                        <li>Check whether outputs from each step are as expected; fix the error and repeat step 1 and 2 if not</li>
+                        <li>Follow this link to build a workflow</li>
+                        </ol><br />
                         Example workflows:<br />
-                        1. GLM<br />
-                        2. Annotation<br />
-                        3. Variant calling<br />
-                        4. Expression<br />
-                        5. Methylation<br />
-                        6. sRNA<br />
-                        7. Chip-seq<br />
+			<ul className="list">
+                        <li>GLM</li>
+                        <li>Annotation</li>
+                        <li>Variant calling</li>
+                        <li>Expression</li>
+                        <li>Methylation</li>
+                        <li>sRNA</li>
+                        <li>Chip-seq</li>
+			</ul>
                         </div>
                         </div>
 
