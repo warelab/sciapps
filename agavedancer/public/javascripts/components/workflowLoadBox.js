eb95f68c4f5f2576cc31ba9d2789c453d51bc929
zhlu9890 <zhenyuan.lu@gmail.com>
2017-02-15 23:43:11 -0500

    change workflowLoadBox

--- agavedancer/public/javascripts/components/workflowLoadBox.js
+++ agavedancer/public/javascripts/components/workflowLoadBox.js
@@ -4,14 +4,20 @@ import React from 'react';
 import Reflux from 'reflux';
 import WorkflowStore from '../stores/workflowStore.js';
 import WorkflowActions from '../actions/workflowActions.js';
-import {Input, Button} from 'react-bootstrap';
+import {Modal, Input, Button} from 'react-bootstrap';
 import utilities from '../libs/utilities.js';
 
 const WorkflowLoadBox=React.createClass({
+	mixins: [Reflux.connect(WorkflowStore, 'workflowStore')],
+	
 	getInitialState: function() {
 		return {
 			value: ''
 		};
+	},
+
+	hideWorkflowLoadBox: function() {
+		WorkflowActions.hideWorkflowLoadBox();
 	},
 
 	handleValueChange: function(event) {
@@ -19,9 +25,12 @@ const WorkflowLoadBox=React.createClass({
 		utilities.readAsText(event.target, function(e) {
 			WorkflowActions.setWorkflowSteps(JSON.parse(e.target.result));
 		}.bind(this));
+		this.hideWorkflowLoadBox();
 	},
 
 	render: function() {
+		let workflowStore=this.state.workflowStore;
+		let showWorkflowLoadBox=workflowStore.showWorkflowLoadBox;
 		let props={
 			type: 'file',
 			value: this.state.value,
@@ -30,7 +39,14 @@ const WorkflowLoadBox=React.createClass({
 			onChange: this.handleValueChange
 		}
 		let markup=(
-			<Input {...props} />
+			<Modal show={showWorkflowLoadBox} onHide={this.hideWorkflowLoadBox}>
+				<Modal.Body>
+					<Input {...props} />
+				</Modal.Body>
+				<Modal.Footer>
+					<Button onClick={this.hideWorkflowLoadBox}>Close</Button>
+				</Modal.Footer>
+			</Modal>
 		);
 		return markup;
 	}
