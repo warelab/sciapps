5f63659cc57a773f6b581dd83fce451933fc384b
zhlu9890 <zhenyuan.lu@gmail.com>
2018-04-12 11:58:27 -0400

    for demo

--- agavedancer/public/javascripts/components/header.js
+++ agavedancer/public/javascripts/components/header.js
@@ -135,8 +135,8 @@ const Header=React.createClass({
 		let userGreeting, userMenu, workflowMenu;
 		let workflowMenuItem=[
 			<MenuItem key='showWorkflowBuilder' eventKey='showWorkflowBuilder' onSelect={this.showWorkflowBuilder}><Glyphicon glyph='wrench' /> Build a workflow</MenuItem>,
-			<MenuItem key='showWorkflowLoader' eventKey='showWorkflowLoader' onSelect={this.showWorkflowLoader}><Glyphicon glyph='cloud-upload' /> Load a workflow</MenuItem>,
-			<MenuItem key='showWorkflows' eventKey='showWorkflows' onSelect={this.showWorkflows}><Glyphicon glyph='th-list' /> Public workflows</MenuItem>
+			//<MenuItem key='showWorkflowLoader' eventKey='showWorkflowLoader' onSelect={this.showWorkflowLoader}><Glyphicon glyph='cloud-upload' /> Load a workflow</MenuItem>,
+			//<MenuItem key='showWorkflows' eventKey='showWorkflows' onSelect={this.showWorkflows}><Glyphicon glyph='th-list' /> Public workflows</MenuItem>
 		];
 		if (user.logged_in) {
 			userGreeting=<NavItem eventKey='greeting'><Glyphicon glyph='user' /> Hi, {user.username}!</NavItem>;
