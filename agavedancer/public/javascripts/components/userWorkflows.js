9238f32e535eb1a4df4bdd121474d0f04e030f34
Liya <wangli@cshl.edu>
2018-08-20 11:10:29 -0400

    Minor changes on user workflow page

--- agavedancer/public/javascripts/components/userWorkflows.js
+++ agavedancer/public/javascripts/components/userWorkflows.js
@@ -102,7 +102,7 @@ const UserWorkflows=React.createClass({
 		return (
 			<ButtonGroup>
 				<Button key='load' bsStyle='success' onClick={this.handleLoad}><Glyphicon glyph='repeat'/> Load</Button>
-				<Button key='view' bsStyle='warning' onClick={this.showWorkflowDiagram}><Glyphicon glyph='modal-window'/> Diagram</Button>
+				<Button key='view' bsStyle='warning' onClick={this.showWorkflowDiagram}><Glyphicon glyph='modal-window'/> Visualize</Button>
         <Button key='share' bsStyle='info' onClick={this.handleShare}><Glyphicon glyph='link'/> Share</Button>
 				<Button key='delete' bsStyle='danger' onClick={this.handleDeleteRow}><Glyphicon glyph='trash'/> Delete</Button>
 			</ButtonGroup>
