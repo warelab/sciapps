fbf551cc238a5178cf0bbaf9ddf62e256f4fcfbf
Liya <wangli@cshl.edu>
2019-10-26 09:50:44 -0400

    Minor changes in public workflow names

--- agavedancer/public/javascripts/components/workflows.js
+++ agavedancer/public/javascripts/components/workflows.js
@@ -17,8 +17,8 @@ var workflowItems = [
 	{ workflow_id: 4, name: "RNA-Seq", description: "Gene level differential expression analysis with STAR, RSEM, and Ebseq" },
         { workflow_id: 5, name: "RNA-Seq2", description: "Isoform level differential expression analysis with STAR, StringTie, and Ballgown" },
         { workflow_id: 6, name: "RNA-Seq3", description: "Isoform level differential expression analysis with HISAT2, StringTie, and Ballgown" },
-	{ workflow_id: 7, name: "Methyl-seq", description: "Differential methylation analysis with Bismark and Bisukit" },
-	{ workflow_id: 8, name: "BSA-seq", description: "Performing Bulked Segregant Analysis with next generation sequencing data" },
+	{ workflow_id: 7, name: "Methyl-Seq", description: "Differential methylation analysis with Bismark and Bisukit" },
+	{ workflow_id: 8, name: "BSA-Seq", description: "Performing Bulked Segregant Analysis with next generation sequencing data" },
 ];
 
 const publicWorkflows=React.createClass({
